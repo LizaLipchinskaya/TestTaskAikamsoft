241c92e5708232adce8f294af904af36790b77b1
Елизавета Липчинская <lip_liz@mail.ru>
2022-08-07 23:03:56 +0700

    подключила бд к проекту

---
+++ src/main/java/controller/Controller.java
@@ -0,0 +1,37 @@
+package controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Controller {
+    public static final String URL = "jdbc:postgresql://localhost:5432/storeAikamsoft";
+    public static final String USERNAME = "postgres";
+    public static final String PASSWORD = "Hello2015";
+    public static final String JDBC_POSTGRES_DRIVER = "org.postgresql.Driver";
+
+    private Connection connection;
+
+    public Controller() {
+        try {
+            Class.forName(JDBC_POSTGRES_DRIVER);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public Connection getConnection()  {
+        if (connection != null) {
+            return connection;
+        }
+
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return connection = null;
+        }
+
+        return connection;
+    }
+}
