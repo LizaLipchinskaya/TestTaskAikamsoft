8e419e4dabf5304bdc2414f5a9cb306e07d0b51a
Елизавета Липчинская <lip_liz@mail.ru>
2022-08-09 00:32:13 +0700

    промежуточный старт программы

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,8 +1,23 @@
-import controller.Controller;
+import org.json.simple.parser.ParseException;
+import parser.SearchParser;
+
+import java.io.File;
+import java.io.IOException;
 
 public class Application {
 
     public static void main(String[] args) {
-        Controller controller = new Controller();
+        switch (args[1]) {
+            case "search" :
+                SearchParser searchParser = new SearchParser();
+                try {
+                    searchParser.parse(new File(args[2]));
+                } catch (IOException | ParseException e) {
+                    e.printStackTrace();
+                }
+                break;
+            case "stat" :
+                break;
+        }
     }
 }
