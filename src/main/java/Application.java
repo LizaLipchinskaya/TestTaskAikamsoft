79e1cee68b23277fa06445aa36856ce0deabe67f
Елизавета Липчинская <lip_liz@mail.ru>
2022-08-09 23:27:27 +0700

    добавила парсер stat

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,23 +1,36 @@
+import controller.Controller;
 import org.json.simple.parser.ParseException;
 import parser.SearchParser;
+import parser.StatParser;
 
 import java.io.File;
 import java.io.IOException;
 
 public class Application {
 
+    private static final Controller controller = new Controller();
+
     public static void main(String[] args) {
         switch (args[1]) {
-            case "search" :
-                SearchParser searchParser = new SearchParser();
+            case "search" -> {
+                SearchParser searchParser = new SearchParser(controller);
                 try {
                     searchParser.parse(new File(args[2]));
                 } catch (IOException | ParseException e) {
                     e.printStackTrace();
                 }
-                break;
-            case "stat" :
-                break;
+            }
+            case "stat" -> {
+                StatParser statParser = new StatParser(controller);
+                try {
+                    statParser.parse(new File(args[2]));
+                } catch (IOException | ParseException e) {
+                    e.printStackTrace();
+                }
+            }
+            default -> {
+                //...
+            }
         }
     }
 }
--- src/main/java/parser/StatParser.java
+++ src/main/java/parser/StatParser.java
@@ -1,15 +1,24 @@
 package parser;
 
+import controller.Controller;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import repository.StatRepository;
 
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 
 public class StatParser {
+
+    private final StatRepository statRepository;
+
+    public StatParser(Controller controller) {
+        statRepository = new StatRepository(controller);
+    }
 
     public void parse(File file) throws IOException, ParseException {
         JSONParser jsonParser = new JSONParser();
@@ -18,8 +27,35 @@ public class StatParser {
         LocalDateTime startDate = (LocalDateTime) jsonObject.get("startDate");
         LocalDateTime endDate = (LocalDateTime) jsonObject.get("endDate");
 
-//        Gson gson = new Gson();
-//        JsonReader reader = new JsonReader(new FileReader(file));
-//        Statistic statistic = gson.fromJson(reader, Statistic.class);
+        int countOfWorkingDay = countWorkingDay(startDate, endDate);
+
+    }
+
+    private int countWorkingDay(LocalDateTime start, LocalDateTime end) {
+        int count = 0;
+
+        if (start.equals(end)) {
+            if (isWorkingDay(start)) {
+                count++;
+            }
+            return count;
+        }
+
+        while (start.isBefore(end)) {
+            if (isWorkingDay(start)) {
+                count++;
+            }
+            start = start.plusDays(1);
+        }
+
+        if (isWorkingDay(end)) {
+            count++;
+        }
+
+        return count;
+    }
+
+    private boolean isWorkingDay(LocalDateTime date) {
+        return date.getDayOfWeek() != DayOfWeek.SATURDAY && date.getDayOfWeek() != DayOfWeek.SUNDAY;
     }
 }
