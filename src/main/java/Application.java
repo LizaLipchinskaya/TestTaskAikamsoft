73af32c0b37ec58b16092e0e0f8e54adabef9853
Елизавета Липчинская <lip_liz@mail.ru>
2022-08-10 23:13:27 +0700

    добавлен вывод в json файл

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,21 +1,29 @@
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import controller.Controller;
+import dto.SearchDto;
 import org.json.simple.parser.ParseException;
 import parser.SearchParser;
 import parser.StatParser;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.Writer;
 
 public class Application {
 
     private static final Controller controller = new Controller();
 
     public static void main(String[] args) {
-        switch (args[1]) {
+        Gson gson = new GsonBuilder().setPrettyPrinting().serializeNulls().create();
+
+        switch (args[0]) {
             case "search" -> {
                 SearchParser searchParser = new SearchParser(controller);
-                try {
-                    searchParser.parse(new File(args[2]));
+                try(Writer writer = new FileWriter(args[2])) {
+                    SearchDto searchDto = searchParser.parse(new File(args[1]));
+                    gson.toJson(searchDto, writer);
                 } catch (IOException | ParseException e) {
                     e.printStackTrace();
                 }
